e010857b7bd3607bdd678e4ae62c670d227ad649
Edvard Sire <edvardsire@protonmail.com>
2022-06-02 22:22:56 +0200

    Update pluss-og-minus.ino

--- pluss-og-minus.ino
+++ pluss-og-minus.ino
@@ -1,15 +1,14 @@
 void setup() {
-  pinMode(2, OUTPUT); //control winding-pair A
-  pinMode(3, OUTPUT); //control winding-pair B
-  pinMode(4, OUTPUT); //control winding-pair C
-  pinMode(5, OUTPUT); //use as +5V for A
-  pinMode(6, OUTPUT); //use as +5V for B
-  pinMode(7, OUTPUT); //use as +5V for C
+  pinMode(2, OUTPUT); //winding-pair A
+  pinMode(3, OUTPUT); //winding-pair B
+  pinMode(4, OUTPUT); //winding-pair C
+  pinMode(5, OUTPUT); //+5V for A
+  pinMode(6, OUTPUT); //+5V for B
+  pinMode(7, OUTPUT); //+5V for C
 }
 
 void loop() {
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
-
 }
